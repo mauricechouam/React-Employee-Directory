4b1375a96e9614bfc5edc21b47d65acd33a67108
mauricechouam <lansichouamou@gmail.com>
2020-08-16 23:03:38 -0700

    added Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <p>Click on Carrots to filter by heading or use the search box to narrow your results</p>
+            </div>
+    )
+}
+export default Header;
